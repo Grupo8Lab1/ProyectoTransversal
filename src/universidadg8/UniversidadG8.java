66aaee960331cbdf3e64ec041dde7a1d2212f048
Ni-luteno <ni.luteno@gmail.com>
2022-10-14 18:05:46 -0300

    Initial commit

---
+++ src/universidadg8/UniversidadG8.java
@@ -0,0 +1,16 @@
+
+
+package universidadg8;
+
+/**
+ *
+ * @author Santi
+ */
+public class UniversidadG8 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Test
+    }
+
+}
