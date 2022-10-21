92f45c526dbe02d6c064a0c439cad0ecdac316da
Ni-luteno <ni.luteno@gmail.com>
2022-10-21 17:40:34 -0300

    MateriaData

--- src/persistencia/MateriaData.java
+++ src/persistencia/MateriaData.java
@@ -1,6 +1,12 @@
 package persistencia;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import universidadg8.entidades.Alumno;
 import universidadg8.entidades.Materia;
 
 public class MateriaData {
@@ -10,26 +16,120 @@ public class MateriaData {
     public MateriaData() {
         this.con = (Conexion) Conexion.getConexion();
     }
-    
-    public void guardarMateria(Materia materia){
+
+    public void guardarMateria(Materia materia) {
+        String sql = "INSERT INTO materia (nombre, anio, estado) VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();//insert, update, delete
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+
+                materia.setId_materia(rs.getInt(1));
+
+                JOptionPane.showMessageDialog(null, "Materia agregada exitosamente");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "MateriaData Sentencia SQL erronea-AgregarMateria");
+        }
     }
 
-    public ArrayList<Materia> obtenerMaterias(){
-        
-        return null;
-    }
-    
-    public Materia obtenerMateriaPorId(int id){
+    public ArrayList<Materia> obtenerMaterias() {
 
-        return null;
+        ArrayList<Materia> listaTemp = new ArrayList();
+
+        String sql = "SELECT * FROM materia WHERE estado = 1";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();//select
+
+            while (rs.next()) {
+
+                Materia m = new Materia();
+
+                m.setId_materia(rs.getInt(""));
+                m.setNombre(rs.getString("nombre"));
+                m.setAnio(rs.getInt("anio"));
+                m.setEstado(rs.getBoolean("estado"));
+
+                listaTemp.add(m);
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "MateriaData Sentencia SQL erronea-ObtenerMaterias");
+        }
+        return listaTemp;
+
     }
-    
-    public void borrarMateria(int id){
-    
+
+    public Materia obtenerMateriaPorId(int idMateria) {
+
+        String sql = "SELECT * FROM materia WHERE estado = 1 AND idMateria = ?";
+        Materia m = new Materia();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idMateria);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                m.setId_materia(idMateria);
+                m.setNombre(rs.getString("nombre"));
+                m.setAnio(rs.getInt("anio"));
+                m.setEstado(rs.getBoolean("estado"));
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "MateriaData Sentencia SQL erronea-obtenerMateriaPorID");
+        }
+        return m;
     }
-    
-    public void actualizaMateria(Materia materia){
-    
+
+    public void borrarMateria(int id) {
+        String sql = "UPDATE materia SET estado=0 WHERE idMateria=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();//
+
+            JOptionPane.showMessageDialog(null, "Se elimino la materia correctamente");
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "MateriaData Sentencia SQL erronea-borrarMateria");
+        }
+    }
+
+    public void actualizaMateria(Materia materia) {
+        String sql = "UPDATE alumnos SET dni=?, apellido = ?, nombre = ?, fechaNacimiento = ?, estado=? WHERE idAlumno=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();//insert, update, delete
+            ps.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Datos de la materia actualizados");
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "MateriaData Sentencia SQL erronea-actualizarMateria");
+        }
     }
 
 }
