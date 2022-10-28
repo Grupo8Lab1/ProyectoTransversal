e6b589146bcdb88ab1acbdab942c83542ac06ace
Ni-luteno <ni.luteno@gmail.com>
2022-10-28 17:32:43 -0300

    Emprolijando y dejo testing pre-setteado

--- src/persistencia/MateriaData.java
+++ src/persistencia/MateriaData.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
--- src/universidadg8/UniversidadG8.java
+++ src/universidadg8/UniversidadG8.java
@@ -1,10 +1,7 @@
 package universidadg8;
 
-import java.sql.Connection;
 import java.time.LocalDate;
-import static java.time.LocalDate.now;
 import persistencia.AlumnoData;
-import persistencia.Conexion;
 import persistencia.InscripcionData;
 import persistencia.MateriaData;
 import universidadg8.entidades.Alumno;
@@ -23,7 +20,42 @@ public class UniversidadG8 {
         MateriaData md = new MateriaData();
         AlumnoData ad = new AlumnoData();
         InscripcionData id = new InscripcionData();
-
         //Testing
+
+        Alumno a = new Alumno(123456789, "apellido", "nombre", LocalDate.now(), true);
+        Materia m = new Materia("MateriaPrueba", 3, true);
+        Inscripcion i = new Inscripcion(a, m, 10);
+
+//Testing AlumnoData
+/*
+ad.guardarAlumno(a);
+System.out.println(ad.obtenerAlumnos());
+System.out.println(ad.obtenerAlumnoPorId(3));
+a = ad.obtenerAlumnoPorId(3);
+a.setDni(1111111111);
+ad.actualizaAlumno(a);
+System.out.println(ad.obtenerAlumnoPorId(3));
+         */
+//Testing MateriaData
+
+        /*        
+md.guardarMateria(m);
+System.out.println(md.obtenerMaterias());
+System.out.println(md.obtenerMateriaPorId(3));
+m = md.obtenerMateriaPorId(3);
+m.setAnio(20);
+md.actualizaMateria(m);
+System.out.println(md.obtenerMateriaPorId(3));
+         */
+//Testing InscripcionData
+
+        /*        
+id.guardarInscripcion(i);
+id.obtenerAlumnosInscriptos(m);
+id.obtenerMateriasInscriptas(a);
+id.obtenerMateriasNoInscriptas(a);
+id.actualizarNota(0, 0, 0);}
+id.borrarInscripcion(0, 0);
+         */
     }
 }
