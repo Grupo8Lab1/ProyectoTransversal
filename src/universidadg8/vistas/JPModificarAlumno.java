03bf8c61efc3ed2859e228ccdec4227038b3eaed
Ni-luteno <ni.luteno@gmail.com>
2022-11-03 21:15:23 -0300

    JPanel ActualizarAlumno (vista boba)

--- src/universidadg8/vistas/JPModificarAlumno.java
+++ src/universidadg8/vistas/JPModificarAlumno.java
@@ -26,19 +26,145 @@ public class JPModificarAlumno extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jTextField1 = new javax.swing.JTextField();
+        jlTitulo2ActualizarAlum = new javax.swing.JLabel();
+        jlTituloActualizarAlum = new javax.swing.JLabel();
+        jcbAlumnosActualizarAlumno = new javax.swing.JComboBox<>();
+        jlNomActualizarAlumno = new javax.swing.JLabel();
+        jlApeActualizarAlumno = new javax.swing.JLabel();
+        jlDNIActualizarAlumno = new javax.swing.JLabel();
+        jlFechaActualizarAlumno = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jtfNombreActualizarAlumno = new javax.swing.JTextField();
+        jtfApeActualizarAlumno = new javax.swing.JTextField();
+        jtfDNIActualizarAlumno = new javax.swing.JTextField();
+        jtfIDAlumActualizarAlumno = new javax.swing.JTextField();
+        jDaChActualizarAlumno = new com.toedter.calendar.JDateChooser();
+        jbActualizarActualizarAlumno = new javax.swing.JButton();
+
+        jTextField1.setText("jTextField1");
+
+        setBorder(javax.swing.BorderFactory.createTitledBorder("Actualizar Datos"));
+        setPreferredSize(new java.awt.Dimension(369, 353));
+
+        jlTitulo2ActualizarAlum.setFont(new java.awt.Font("Yu Gothic Medium", 0, 18)); // NOI18N
+        jlTitulo2ActualizarAlum.setText("Datos");
+
+        jlTituloActualizarAlum.setFont(new java.awt.Font("Yu Gothic Medium", 0, 18)); // NOI18N
+        jlTituloActualizarAlum.setText("Alumno");
+
+        jcbAlumnosActualizarAlumno.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jlNomActualizarAlumno.setText("Nombre");
+
+        jlApeActualizarAlumno.setText("Apellido");
+
+        jlDNIActualizarAlumno.setText("DNI");
+
+        jlFechaActualizarAlumno.setText("Fecha Nacimiento");
+
+        jLabel1.setText("Legajo");
+
+        jtfDNIActualizarAlumno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtfDNIActualizarAlumnoActionPerformed(evt);
+            }
+        });
+
+        jtfIDAlumActualizarAlumno.setEditable(false);
+
+        jbActualizarActualizarAlumno.setText("Actualizar Datos");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 369, Short.MAX_VALUE)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jbActualizarActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(23, 23, 23)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jlNomActualizarAlumno)
+                                    .addComponent(jlFechaActualizarAlumno)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jlDNIActualizarAlumno, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jlApeActualizarAlumno, javax.swing.GroupLayout.Alignment.LEADING))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jlTitulo2ActualizarAlum, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jlTituloActualizarAlum, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jcbAlumnosActualizarAlumno, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jtfApeActualizarAlumno, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jtfDNIActualizarAlumno, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jtfIDAlumActualizarAlumno, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jtfNombreActualizarAlumno, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jDaChActualizarAlumno, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 353, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlTituloActualizarAlum, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jcbAlumnosActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jlTitulo2ActualizarAlum)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlNomActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfNombreActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlApeActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfApeActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlDNIActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfDNIActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfIDAlumActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlFechaActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jDaChActualizarAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jbActualizarActualizarAlumno, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jtfDNIActualizarAlumnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtfDNIActualizarAlumnoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtfDNIActualizarAlumnoActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.toedter.calendar.JDateChooser jDaChActualizarAlumno;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton jbActualizarActualizarAlumno;
+    private javax.swing.JComboBox<String> jcbAlumnosActualizarAlumno;
+    private javax.swing.JLabel jlApeActualizarAlumno;
+    private javax.swing.JLabel jlDNIActualizarAlumno;
+    private javax.swing.JLabel jlFechaActualizarAlumno;
+    private javax.swing.JLabel jlNomActualizarAlumno;
+    private javax.swing.JLabel jlTitulo2ActualizarAlum;
+    private javax.swing.JLabel jlTituloActualizarAlum;
+    private javax.swing.JTextField jtfApeActualizarAlumno;
+    private javax.swing.JTextField jtfDNIActualizarAlumno;
+    private javax.swing.JTextField jtfIDAlumActualizarAlumno;
+    private javax.swing.JTextField jtfNombreActualizarAlumno;
     // End of variables declaration//GEN-END:variables
 }
